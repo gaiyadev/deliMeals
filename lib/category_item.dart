eb97b44b568a859f18ac28cba60450d2e5bfc3de
gaiyadev <gaiyaobed94@gmail.com>
2020-08-22 19:02:55 +0100

    Feat: Padding date to a new screen

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,30 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:navigation/screen/category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
+  final String id;
   final String title;
   final Color color;
 
   CategoryItem({
+    this.id,
     this.title,
     this.color,
   });
+
+  void getCategory(BuildContext context) {
+    Navigator.of(context).push(
+      MaterialPageRoute(
+        builder: (_) {
+          return CategoryMealScreen(
+            categoryId: id,
+            categoryTitle: title,
+          );
+        },
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(15.0),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.headline6,
-      ),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(15.0),
-        gradient: LinearGradient(
-          colors: [
-            color.withOpacity(.7),
-            color,
-          ],
-          begin: Alignment.topLeft,
-          end: Alignment.bottomLeft,
+    return InkWell(
+      onTap: () => getCategory(context),
+      child: Container(
+        padding: const EdgeInsets.all(15.0),
+        child: Text(
+          title,
+          style: Theme.of(context).textTheme.headline6,
+        ),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(15.0),
+          gradient: LinearGradient(
+            colors: [
+              color.withOpacity(.7),
+              color,
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomLeft,
+          ),
         ),
       ),
     );
